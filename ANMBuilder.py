15a60f5eaef9b6d8235fdf837384d71825d3d8f0
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2023-04-27 12:36:45 -0700

    Full color animations are now supported.

--- ANMBuilder.py
+++ ANMBuilder.py
@@ -2,7 +2,7 @@ from PIL import Image # 9.4.0-2
 import struct
 from enum import Enum
 
-def addColor( endian, r, g, b ):
+def addColor( endian, b, g, r ):
     r_bit = min( int( r * 32.0 ), 31 )
     g_bit = min( int( g * 32.0 ), 31 )
     b_bit = min( int( b * 32.0 ), 31 )
@@ -11,11 +11,11 @@ def addColor( endian, r, g, b ):
 
     return bytearray( struct.pack( "{}H".format( endian ), bitfield ) )
 
-def makeHeader( endian, number_of_frames ):
+def makeHeader( endian, number_of_frames, given_palette ):
     data = bytearray( struct.pack( "{}I".format( endian ), number_of_frames ) )
 
     for i in range(0, 256):
-        data += addColor( endian, i / 255.0, i / 255.0, i / 255.0 )
+        data += addColor( endian, given_palette[i * 3] / 255.0, given_palette[i * 3 + 1] / 255.0, given_palette[i * 3 + 2] / 255.0 )
 
     return data;
 
@@ -37,16 +37,11 @@ class Platform( Enum ):
     Windows = 1
     Macintosh = 2
 
-def writeFNTFile( reference_image_path : str, output_fnt_path : str, kind : Platform ):
-    # colorful_image = Image.open( reference_image_path )
+def writeFNTFile( reference_image_path : str, reference_color_palette : str, output_fnt_path : str, kind : Platform ):
+    source_palette_img = Image.open( reference_color_palette )
+    quant_img = source_palette_img.quantize()
 
-    #if colorful_image.width != 256:
-    #    print( "This format does not support a width of ", colorful_image.width )
-    #    print( "The width has to be ", 256 )
-    #    exit()
-
-    # There is only one color channel.
-    # img = colorful_image.getchannel( 0 )
+    palette = quant_img.getpalette()
 
     status_endian = '@'
 
@@ -57,17 +52,17 @@ def writeFNTFile( reference_image_path : str, output_fnt_path : str, kind : Plat
     else:
         status_endian = '>'
 
-    data = makeHeader( endian = status_endian, number_of_frames = 30)
+    data = makeHeader( endian = status_endian, number_of_frames = 30, given_palette = palette)
 
     for i in range(1, 31):
         p = reference_image_path + "/{:04d}.png".format( i )
         colorful_image = Image.open( p )
-        img = colorful_image.getchannel( 0 )
+        img = colorful_image.quantize( palette = quant_img )
         data += writeSingleFrame( img )
 
     new_file = open( output_fnt_path, "wb" )
     new_file.write( data )
 
-writeFNTFile( reference_image_path = "Frames", output_fnt_path = "win.canm", kind = Platform.Windows )
-# writeFNTFile( reference_image_path = "Frames", output_fnt_path = "mac.canm", kind = Platform.Macintosh )
+writeFNTFile( reference_image_path = "Frames", reference_color_palette = "Frames/0003.png", output_fnt_path = "win.canm", kind = Platform.Windows )
+writeFNTFile( reference_image_path = "Frames", reference_color_palette = "Frames/0003.png", output_fnt_path = "mac.canm", kind = Platform.Macintosh )
 # writeFNTFile( reference_image_path = "Frames", output_fnt_path = "ps1.canm", kind = Platform.Playstation )
